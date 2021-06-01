72f64700a083d7af5bb645349a4bc385046ca6b9
yurykurylovich <yury.kurylovich@gmail.com>
2021-06-01 11:22:59 +0300

    Objects course chapter 5 task done

---
+++ Course-08 [Objects]/chapter-05 getSortedNames.js
@@ -0,0 +1,33 @@
+/* getSortedNames.js
+Write and export by default getSortedNames() function that takes an array of objects with users' data and returns an array of users' names sorted alphabetically.
+*/
+
+// solution
+const getSortedNames = (arr) => {
+  let result = [];
+  for (let item of arr) {
+    result.push(item.name)
+  }
+  return result.sort();
+}
+// test
+const users = [
+  { name: "Bronn", gender: "male", birthday: "1973-03-23" },
+  { name: "Reigar", gender: "male", birthday: "1973-11-03" },
+  { name: "Eiegon", gender: "male", birthday: "1963-11-03" },
+  { name: "Sansa", gender: "female", birthday: "2012-11-03" },
+];
+
+console.log(getSortedNames(users));
+// ["Bronn", "Eiegon", "Reigar", "Sansa"]
+
+const data = [
+  { name: "Jon", gender: "male", birthday: "1980-11-03" },
+  { name: "Robb", gender: "male", birthday: "1980-05-14" },
+  { name: "Tisha", gender: "female", birthday: "2012-11-03" },
+  { name: "Rick", gender: "male", birthday: "2012-11-03" },
+  { name: "Joffrey", gender: "male", birthday: "1999-11-03" },
+  { name: "Edd", gender: "male", birthday: "1973-11-03" },
+];
+console.log(getSortedNames(data));
+// ["Edd", "Joffrey", "Jon", "Rick", "Robb", "Tisha"]
