dc55fc73d68f6c2af35a3ff392c9a425adaa390e
yurykurylovich <yury.kurylovich@gmail.com>
2021-06-27 19:25:21 +0300

    Finished Asynchronous programming course task 3

---
+++ Course-17 [Asynchronous programming]/task-03 compareFileSizes.js/compareFileSizes.js
@@ -0,0 +1,43 @@
+/* info.js
+Реализуйте и экспортируйте асинхронную функцию compareFileSizes, которая сравнивает размеры двух файлов.
+Если первый больше второго, то она возвращает единицу, если размеры равны, то возвращает ноль, иначе — -1.
+import { compareFileSizes } from './info.js';
+compareFileSizes('file1', 'file2', (_err, result) => console.log(result));
+Подсказка
+Для реализации этого задания, нужно воспользоваться функцией fs.stat, которая использовалась в примерах теории
+Math.sign */
+
+const fs = require('fs')
+const path = require('path')
+
+const compareFileSizes = (file1, file2, callback) => {
+  fs.stat(file1, (err, data1) => {
+    fs.stat(file2, (err, data2) => {
+      const result = Math.sign(data1.size - data2.size)
+      callback(null, result)
+    })
+  })
+}
+
+compareFileSizes('./compareFileSizes.js', './readme.md', (_error, result) => console.log(result))
+
+// const getFileOwners = (dirpath, cb) => {
+//   fs.readdir(dirpath, (_error1, filenames) => {
+//     const readFileStat = (items, result = []) => {
+//       if (items.length === 0) {
+//         // Обработку ошибок пока не рассматриваем
+//         cb(null, result);
+//         return;
+//       }
+//       const [first, ...rest] = items;
+//       console.log(items)
+//       const filepath = path.join(dirpath, first);
+//       fs.stat(filepath, (_error2, stat) => {
+//         readFileStat(rest, [...result, { filename: first, owner: stat.uid }]);
+//       });
+//     };
+//     readFileStat(filenames);
+//   });
+// };
+//
+// getFileOwners('../task-03 compareFileSizes.js', (error, data) => {console.log(data)})
