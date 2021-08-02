8e65f7b80b94f9402f28ff64ba9837e4f01489a5
yurykurylovich <yury.kurylovich@gmail.com>
2021-08-02 23:56:36 +0300

    Finished Frontend Architecture course task 4

---
+++ Course-20 [Frontend Architecture]/task-04 mvc/listGroups.js
@@ -0,0 +1,41 @@
+/* listGroups.js
+Realize an interface of bootstrap's list-group
+https://getbootstrap.com/docs/4.5/components/list-group/#javascript-behavior
+Use on-change library. https://github.com/sindresorhus/on-change
+ */
+
+/// SHOWCASE ON CODEPEN - https://codepen.io/yury-kurylovich/pen/poPZgqv?editors=1010
+
+import onChange from "on-change";
+
+const run = (()=> {
+  const state = {
+    active: "list-home-list"
+  };
+  const watchedState = onChange(state, (path, value) => {
+    render(value);
+  });
+  const listGroups = document.querySelectorAll(".container");
+  listGroups.forEach(group => {
+    let items = document.querySelectorAll(".list-group-item");
+    items.forEach(item => {
+      item.addEventListener("click", (e) => {
+        watchedState.active = item.id;
+      });
+    });
+  });
+})();
+
+const render = (name) => {
+  let item = document.getElementById(name);
+  let list = item.closest(".list-group");
+  let oldActive = list.querySelector(".active");
+  oldActive.classList.remove("active");
+  item.classList.add("active");
+
+  let content = document.querySelector(`[aria-labelledby="${name}"]`);
+  let tab = content.closest(".tab-content");
+  let oldActiveTab = tab.querySelector(".active");
+  oldActiveTab.classList.remove("active", "show");
+  content.classList.add("active", "show");
+};
