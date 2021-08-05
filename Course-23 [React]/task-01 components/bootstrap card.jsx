555ca08ba106ec56b1b8abdc1becbdf7247528ed
Yury Kyrylovich <yury.kurylovich@gmail.com>
2021-08-05 23:12:49 +0300

    Started React course

---
+++ Course-23 [React]/task-01 components/bootstrap card.jsx
@@ -0,0 +1,5 @@
+/* Card.jsx
+Write a realization for the Bootstrap's card component using React in Card.jsx. Export it to index.jsx and render it inside the DOM element with id #container.
+*/
+
+// CODEPEN - 
