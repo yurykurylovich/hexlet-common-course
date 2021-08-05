5df1008130b87ee1bad52e79014fc8905d6f20b5
yurykurylovich <yury.kurylovich@gmail.com>
2021-08-05 13:50:33 +0300

    Finished Frontend Architecture course task 5

---
+++ Course-20 [Frontend Architecture]/task-05 form state/formValidation.js
@@ -0,0 +1,128 @@
+
+/* formValidation.js
+Using yup library (https://github.com/jquense/yup#usage) write validation of the bootstrap's user registration form.
+Use axios for the POST response and onChange library to check all the changes.
+*/
+
+//// SHOWCASE ON CODEPEN - https://codepen.io/yury-kurylovich/pen/PomdQLM
+import * as y from "yup";
+import onChange from "on-change";
+import axios from "axios";
+
+const yup = !y.object ? y.default : y;
+// Never hardcore urls
+const routes = {
+  usersPath: () => "/users",
+};
+
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().required().email(),
+  password: yup.string().required().min(6),
+  passwordConfirmation: yup
+    .string()
+    .required()
+    .oneOf(
+      [yup.ref("password"), null],
+      "Password confirmation doesn't match the password"
+    ),
+});
+
+const errorMessages = {
+  network: {
+    error: "Network problems. Try again.",
+  },
+}
+
+// SOLUTION BEGINNING
+const run = (() => {
+  const form = document.querySelector("form");
+  const button = document.querySelector(".btn");
+  const container = document.querySelector(".container");
+
+  const state = {
+    registrationForm: {
+      state: "invalid",
+      data: {
+        name: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+      },
+      errors: {},
+    }
+  };
+
+  const render = () => {
+    let errors = state.registrationForm.errors;
+    let keys = Object.keys(state.registrationForm.data);
+    if (state.registrationForm.state === "invalid") {
+      keys.forEach((key) => {
+        (errors && errors[key])
+          ? form.elements[key].classList.add("is-invalid")
+          : form.elements[key].classList.remove("is-invalid")
+      })
+    } else {
+      keys.forEach((key) => {
+        form.elements[key].classList.remove("is-invalid")
+      })
+      button.disabled = false;
+    }
+  };
+
+  const watchedState = onChange(state, (path, value) => {
+    let arr = path.split(".");
+    let item = arr[arr.length -1];
+    submitHandler(item);
+    render();
+    if (item === "password") {
+      submitHandler("passwordConfirmation");
+    }
+  });
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    button.disabled = true;
+    let data = state.registrationForm.data;
+    axios.post("/users", data)
+      .then(function (response) {
+        document.body.removeChild(container);
+        document.body.innerHTML = '<div data-container="sign-up">User Created!</div>';
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  })
+
+  const formElements = form.elements;
+  [...formElements].forEach((element) => {
+    element.addEventListener("input", (e) => {
+      let type = element.name;
+      watchedState.registrationForm.data[type] = e.target.value;
+    })
+  })
+
+  const submitHandler = (item) => {
+    schema
+      .validateAt(`${item}`, state.registrationForm.data)
+      .then(() => {
+        state.registrationForm.errors[item] = null;
+        validateAll();
+      })
+      .catch(err => {
+        state.registrationForm.errors[item] = err.errors
+      })
+      .finally(() => render())
+  };
+
+  const validateAll = () => {
+    try {
+      schema.validateSync(state.registrationForm.data, { abortEarly: false });
+      state.registrationForm.state = "valid"
+    } catch (e) {
+      console.log("Validation Error");
+    }
+  }
+})();
+
+
