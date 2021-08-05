2dc445bff64943063cc2936f83551e829e6bcdb1
yurykurylovich <yury.kurylovich@gmail.com>
2021-08-06 00:06:40 +0300

    Finished React course task 1

--- Course-23 [React]/task-01 components/bootstrap card.jsx
+++ Course-23 [React]/task-01 components/bootstrap card.jsx
@@ -2,4 +2,33 @@
 Write a realization for the Bootstrap's card component using React in Card.jsx. Export it to index.jsx and render it inside the DOM element with id #container.
 */
 
-// CODEPEN - 
+// CODEPEN - https://codepen.io/yury-kurylovich/pen/bGWmweo
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+// CARD.JSX
+export default class Card extends React.Component {
+  render() {
+    const crd = (
+      <div className="card">
+        <div className="card-body">
+          <h4 className="card-title">Card title</h4>
+          <p className="card-text">
+            Some quick example text to build on the card
+          </p>
+          <button type="button" className="btn btn-primary">
+            Go somewhere
+          </button>
+        </div>
+      </div>
+    );
+    return crd;
+  }
+}
+
+// INDEX.JSX
+import Card from "./Card.jsx";
+
+const mountNode = document.getElementById("container");
+ReactDOM.render(<Card />, mountNode);
