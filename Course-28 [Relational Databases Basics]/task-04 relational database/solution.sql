68bbb67f13b3c4624dcc5516ea78ed4149833b15
yurykurylovich <yury.kurylovich@gmail.com>
2021-09-12 23:46:07 +0300

    Finished Relational DB task 4

---
+++ Course-28 [Relational Databases Basics]/task-04 relational database/solution.sql
@@ -0,0 +1,14 @@
+CREATE TABLE cars (
+    user_first_name varchar(255),
+    brand varchar(255),
+    model varchar(255)
+);
+
+INSERT INTO users VALUES ('one', 'two', '1832-10-11');
+INSERT INTO users VALUES ('three', 'four', '1832-10-11');
+
+INSERT INTO cars VALUES ('one', 'bmw', 'x5');
+INSERT INTO cars VALUES ('one', 'bmw', 'x3');
+INSERT INTO cars VALUES ('three', 'kia', 'rio');
+INSERT INTO cars VALUES ('three', 'kia', 'sorento');
+INSERT INTO cars VALUES ('three', 'kia', 'sportage');
