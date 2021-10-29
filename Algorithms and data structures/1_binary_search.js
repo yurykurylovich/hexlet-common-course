03d6becd61c1d48a61c27709f7aa05f32007c493
yurykurylovich <yury.kurylovich@gmail.com>
2021-10-29 20:37:04 +0300

    Add quick sort and recursive sort algos

--- Algorithms and data structures/1_binary_search.js
+++ Algorithms and data structures/1_binary_search.js
@@ -1,6 +1,6 @@
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const array = [0,1,2,3,4,5,6,7,8,9,10,11,22,23,54,85]
 let count = 0;
-
+// -- Simple binary search
 function binarySearch(arr, item) {
   let start = 0;
   let end = arr.length
@@ -27,3 +27,23 @@ function binarySearch(arr, item) {
 
 console.log(binarySearch(array, 10))
 console.log(count)
+
+
+// -- Recursive binary search
+let recursiveCount = 0;
+function recursiveBinarySearch(array, item, start, end) {
+  let middle = Math.floor((start + end) / 2);
+  recursiveCount += 1;
+  if (item === array[middle]) {
+    return middle;
+  }
+  if (item < array[middle]) {
+    return recursiveBinarySearch(array, item, 0, middle - 1);
+  } else {
+    return recursiveBinarySearch(array, item, middle + 1, end)
+  }
+}
+
+console.log("recursiveSearchResult = ",recursiveBinarySearch(array, 54, 0, array.length))
+
+
